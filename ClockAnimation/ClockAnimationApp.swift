c0a961783465af067544fac9d73c09c025736a8a
Elaidzha1940 <eli080293@gmail.com>
2023-09-27 20:35:45 +0300

    ClockAnimation

---
+++ ClockAnimation/ClockAnimationApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ClockAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.09.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ClockAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: ClockAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.09.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
