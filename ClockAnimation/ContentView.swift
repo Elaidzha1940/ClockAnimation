5a58103f3e93dcb897e78b0a3d8644b80a159674
Elaidzha1940 <eli080293@gmail.com>
2023-09-29 16:37:04 +0300

    Updated more func

--- ClockAnimation/ContentView.swift
+++ ClockAnimation/ContentView.swift
@@ -21,6 +21,26 @@ struct ContentView: View {
         }
     }
     
+    struct MinutesHand: Shape {
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.midX, y: 240))
+            return path
+        }
+    }
+    
+    struct HoursHand: Shape {
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.midX, y: 200))
+            return path
+        }
+    }
+    
     var body: some View {
         
         ZStack {
@@ -30,6 +50,11 @@ struct ContentView: View {
                             StrokeStyle(lineWidth: 10))
                 .foregroundColor(.white)
                 .padding()
+            
+            SecondsHand()
+                .stroke(style:
+                            StrokeStyle(lineWidth: 10))
+                .foregroundColor(.white)
             
         }
         .preferredColorScheme(.dark)
