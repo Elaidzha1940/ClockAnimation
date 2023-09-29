c423886d1621035ebf35272792c237125e996900
Elaidzha1940 <eli080293@gmail.com>
2023-09-29 17:20:03 +0300

    Updated angleRotationSeconds / angleRotationMinutes / angleRotationHour

--- ClockAnimation/ContentView.swift
+++ ClockAnimation/ContentView.swift
@@ -9,41 +9,46 @@
 
 import SwiftUI
 
+struct SecondsHand: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: 370))
+        return path
+    }
+}
+
+struct MinutesHand: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: 240))
+        return path
+    }
+}
+
+struct HourHand: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: 200))
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State var angleRotationSeconds = 0
+    @State var angleRotationMinutes = 40
+    @State var angleRotationHour = -50
     
-    struct SecondsHand: Shape {
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.midX, y: 250))
-            return path
-        }
-    }
-    
-    struct MinutesHand: Shape {
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.midX, y: 240))
-            return path
-        }
-    }
-    
-    struct HoursHand: Shape {
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.midX, y: 200))
-            return path
-        }
-    }
+    @State var seconds = 1.0
+    @State var minutes = 60.0
+    @State var hour = 360.0
     
     var body: some View {
-        @State var angleRotationSeconds = 0
-        @State var seconds = 1.0
         
         ZStack {
             
@@ -53,6 +58,18 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .padding()
             
+            HourHand()
+                .stroke(style:
+                            StrokeStyle(lineWidth: 10))
+                .foregroundColor(.white)
+                .rotationEffect(.init(degrees: Double(angleRotationHour)))
+            
+            MinutesHand()
+                .stroke(style:
+                            StrokeStyle(lineWidth: 10))
+                .foregroundColor(.white)
+                .rotationEffect(.init(degrees: Double(angleRotationMinutes)))
+            
             SecondsHand()
                 .stroke(style:
                             StrokeStyle(lineWidth: 10))
@@ -60,15 +77,22 @@ struct ContentView: View {
                 .rotationEffect(.init(degrees: Double(angleRotationSeconds)))
         }
         .onAppear {
-            
             Timer.scheduledTimer(withTimeInterval: seconds, repeats: true) { timer in
                 angleRotationSeconds += 1
             }
+            
+            Timer.scheduledTimer(withTimeInterval: minutes, repeats: true) { timer in
+                angleRotationMinutes += 1
+            }
+            
+            Timer.scheduledTimer(withTimeInterval: hour, repeats: true) { timer in
+                angleRotationHour += 1
+            }
         }
-        .preferredColorScheme(.dark)
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
